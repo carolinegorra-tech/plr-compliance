548e1099691a7fe0acf5c8a9e2fc8ea30b2e1e20
carolinegorra-tech <caroline.gorra@gmail.com>
2026-03-09 11:41:40 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -264,7 +264,7 @@ export default function PLRAnalyzer() {
         top: 0,
         zIndex: 100,
       }}>
-        <MM_LOGO height={120} />
+        <MM_LOGO height={80} />
         <div style={{ textAlign: "right" }}>
           <div style={{ color: "#0A1628", fontSize: "11px", letterSpacing: "3px", marginBottom: "2px", fontWeight: "600" }}>ÁREA TRABALHISTA</div>
           <div style={{ color: "#6B7280", fontSize: "13px", letterSpacing: "1px" }}>Analisador de Compliance PLR</div>
